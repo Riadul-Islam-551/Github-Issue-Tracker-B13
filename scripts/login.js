9e4822dfeab063511820472c6ad6b77b31ece432
Riadul Islam <riadul551@gmail.com>
2026-03-16 16:49:11 +0600

    make the navigation bar of main page

---
+++ scripts/login.js
@@ -0,0 +1,22 @@
+const loginPage = document.getElementById("login-page");
+const mainPage = document.getElementById("main-page");
+
+const userInput = document.getElementById("user-input");
+const passwordInput = document.getElementById("password-input");
+
+const loginButton = document.getElementById("login-btn");
+
+loginButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const user = userInput.value;
+  const password = passwordInput.value;
+
+  console.log(user, password);
+
+  if (user === "admin" && password === "admin123") {
+    loginPage.classList.add("hidden");
+    mainPage.classList.remove("hidden");
+  }
+});
+
+// console.log(userInput);
